8df1321c4f410189dcb2522829b11edf98828614
Sam Mercer <sam@mercerfamily.org.uk>
2020-05-05 17:47:16 +0100

    Add CameraTools

---
+++ Mods/CameraToolsPlugin/CameraToolsPlugin/Plugin.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using UnityEngine;
+
+
+namespace CameraToolsPlugin
+{
+    [BepInPlugin("org.mercer.plugins.CameraTools", "Camera Tools", "1.0.0.0")]
+    [BepInProcess("TaleSpire.exe")]
+    public class CameraTools : BaseUnityPlugin
+    {
+
+        private bool _enabled = false;
+        private bool _orthEnabled = false;
+
+        const BindingFlags flags = BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static;
+        // Awake is called once when both the game and the plug-in are loaded
+        void Awake()
+        {
+            UnityEngine.Debug.Log("Camera Tool loaded");
+        }
+
+        private void ToggleCamera()
+        {
+            try
+            {
+                if (!_enabled) {
+                    UnityEngine.Debug.Log("The Cam overrides are disabled enabling!");
+
+                    if (!CameraController.HasInstance) return;
+
+                    var cam = CameraController.Instance;
+                    UnityEngine.Debug.Log("CAM: " + cam);
+
+                    cam.GetType().GetField("_minTilt", flags)?.SetValue(cam, -100);
+                    cam.GetType().GetField("_maxTilt", flags)?.SetValue(cam, 120.0f);
+                    //cam.GetType().GetField("_maxFov", flags).SetValue(cam, 100f);
+                    this._enabled = true;
+                }
+                else
+                {
+                    UnityEngine.Debug.Log("The Cam overrides are enabled disabling!");
+                    Camera.main.orthographic = false;
+                    Camera.main.orthographicSize = 5f;
+
+                    if (!CameraController.HasInstance) return;
+
+                    var cam = CameraController.Instance;
+                    UnityEngine.Debug.Log("CAM: " + cam);
+
+                    cam.GetType().GetField("_minTilt", flags)?.SetValue(cam, -10);
+                    cam.GetType().GetField("_maxTilt", flags)?.SetValue(cam, 40);
+                    //cam.GetType().GetField("_maxFov", flags).SetValue(cam, 30);
+                    this._enabled = false;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                UnityEngine.Debug.LogError("handled crash");
+                UnityEngine.Debug.LogError(ex.Message);
+                UnityEngine.Debug.LogError(ex.StackTrace);
+                UnityEngine.Debug.LogError(ex.InnerException);
+                UnityEngine.Debug.LogError(ex.Source);
+            }
+        }
+
+        private void toggleOrthographicCamera()
+        {
+            try
+            {
+                if (!_orthEnabled)
+                {
+                    UnityEngine.Debug.Log("The Orthographic overrides are disabled enabling!");
+
+                    Camera.main.orthographic = true;
+                    Camera.main.orthographicSize = 50.0f;
+
+                    if (CameraController.HasInstance)
+                    {
+
+                        var cam = CameraController.Instance;
+                        cam.GetType().GetField("_minTilt", flags)?.SetValue(cam, 0.0f);
+                        cam.GetType().GetField("_maxTilt", flags)?.SetValue(cam, 120.0f);
+                        //cam.GetType().GetField("_maxFov", flags).SetValue(cam, 120.0f);
+                        this._orthEnabled = true;
+                    }
+                    if (SingletonBehaviour<AtmosphereManager>.HasInstance)
+                    {
+                        var atm = SingletonBehaviour<AtmosphereManager>.Instance;
+                        var fields = atm.GetType().GetFields(flags);
+                        AtmosphereApplier aa;
+                        foreach (var field in fields)
+                        {
+                            if (field.Name == "_applier")
+                            {
+                                aa = (AtmosphereApplier)field.GetValue(atm);
+                                //UnityEngine.Debug.Log("Aperture Min: " + t.FieldType.GetField("_dofApertureMin", flags).GetValue(aa));
+                                //UnityEngine.Debug.Log("Aperture Max: " + t.FieldType.GetField("_dofApertureMax", flags).GetValue(aa));
+                                field.FieldType.GetField("_dofApertureMin", flags)?.SetValue(aa, 0.0f);
+                                field.FieldType.GetField("_dofApertureMax", flags)?.SetValue(aa, 1000.0f);
+                            }
+                        }
+
+                    }
+                }
+                else
+                {
+                    UnityEngine.Debug.Log("The Cam overrides are enabled disabling!");
+                    Camera.main.orthographic = false;
+                    Camera.main.orthographicSize = 5f;
+
+                    if (CameraController.HasInstance)
+                    {
+                        var cam = CameraController.Instance;
+                        UnityEngine.Debug.Log("CAM: " + cam);
+
+
+                        cam.GetType().GetField("_minTilt", flags)?.SetValue(cam, -10);
+                        cam.GetType().GetField("_maxTilt", flags)?.SetValue(cam, 40);
+                        cam.GetType().GetField("_maxFov", flags)?.SetValue(cam, 30);
+                        this._orthEnabled = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                UnityEngine.Debug.LogError("handled crash");
+                UnityEngine.Debug.LogError(ex.Message);
+                UnityEngine.Debug.LogError(ex.StackTrace);
+                UnityEngine.Debug.LogError(ex.InnerException);
+                UnityEngine.Debug.LogError(ex.Source);
+            }
+        }
+
+
+        void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.F5))
+            {
+                ToggleCamera();
+            }
+            if (Input.GetKeyUp(KeyCode.F6))
+            {
+                toggleOrthographicCamera();
+            }
+
+            if(_orthEnabled)
+            {
+                if (Input.GetAxis("Mouse ScrollWheel") > 0)
+                {
+                    Camera.main.orthographicSize -= .25f;
+                }
+                else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+                {
+                    Camera.main.orthographicSize += .25f;
+                    
+                }
+            }
+        }
+    }
+}
