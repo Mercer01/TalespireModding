fa2c3191ad40fd910194796859453c94bf845841
h3lls <savagesoul@gmail.com>
2020-05-10 20:56:06 -0700

    Persist character names through board changes
    Also added ModdingUtils to simplify standard plugin functions

--- Mods/ToggleCharacterNames/Plugin.cs
+++ Mods/ToggleCharacterNames/Plugin.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using BepInEx;
-using TMPro;
+﻿using BepInEx;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 namespace ToggleCharacterNames
 {
-    [BepInPlugin("org.mercer.plugins.ToggleCharacterNames", "Toggle Character Names", "1.1.0")]
+    [BepInPlugin("org.mercer.plugins.ToggleCharacterNames", "Toggle Character Names", "1.2.0")]
     [BepInProcess("TaleSpire.exe")]
     public class Plugin : BaseUnityPlugin
     {
@@ -14,47 +12,24 @@ namespace ToggleCharacterNames
         void Awake()
         {
             Debug.Log("Starting Toggle Characters Plugin");
+            ModdingTales.ModdingUtils.Initialize(this);
         }
 
         private void ToggleTextLayer()
         {
-            ToolIndicatorUI.TextLayerEnabled = !_enabled;
             this._enabled = !_enabled;
         }
 
         void Update()
         {
+            if (this._enabled)
+            {
+                // Force it to always be displayed if the user enables the plugin, otherwise ignore it.
+                ToolIndicatorUI.TextLayerEnabled = _enabled;
+            }
             if (Input.GetKeyUp(KeyCode.F4))
             {
                 ToggleTextLayer();
-            }
-        }
-
-        void OnEnable()
-        {
-            SceneManager.sceneLoaded += OnSceneLoaded;
-        }
-
-        void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-        {
-            UnityEngine.Debug.Log("Loading Scene: " + scene.name);
-            TextMeshProUGUI[] texts = FindObjectsOfType<TextMeshProUGUI>();
-
-            for (int i = 0; i < texts.Length; i++)
-            {
-                if (scene.name == "UI" && texts[i].name == "BETA")
-                {
-                    texts[i].text = "INJECTED BUILD - unstable mods";
-                }
-                if (scene.name == "Login" && texts[i].name == "TextMeshPro Text")
-                {
-                    BepInPlugin bepInPlugin = (BepInPlugin)Attribute.GetCustomAttribute(this.GetType(), typeof(BepInPlugin));
-                    if (texts[i].text.EndsWith("</size>"))
-                    {
-                        texts[i].text += "\n\nMods Currently Installed:\n";
-                    }
-                    texts[i].text += "\n" + bepInPlugin.Name + " - " + bepInPlugin.Version;
-                }
             }
         }
     }
